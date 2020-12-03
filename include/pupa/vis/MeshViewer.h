5e559009a6a611bc5560c5527f0c753586fae1af
C.Xie <xiconxi@gmail.com>
2020-12-03 21:56:34 +0800

    games102 hw6 v1

--- include/pupa/vis/MeshViewer.h
+++ include/pupa/vis/MeshViewer.h
@@ -5,10 +5,9 @@
 #pragma once
 
 #include <pmp/SurfaceMesh.h>
+#include <pmp/visualization/SurfaceMeshGL.h>
 #include <pmp/visualization/TrackballViewer.h>
 #include <pmp/algorithms/SurfaceNormals.h>
-#include "MeshPrimitive.h"
-#include "GLMaterial.h"
 
 #include <memory>
 #include <imgui.h>
@@ -34,14 +33,13 @@ public:
     MeshViewer(const char* title, int width, int height, bool showgui = true)
         : TrackballViewer(title, width, height, showgui)
     {
-//        glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
+        glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
         // setup draw modes
         clear_draw_modes();
         add_draw_mode("Points");
         add_draw_mode("Hidden Line");
         add_draw_mode("Smooth Shading");
-        // add help items
-        add_help_item("Backspace", "Reload mesh", 3);
+
 #ifndef __EMSCRIPTEN__
         add_help_item("W", "Write mesh to 'output.off'", 4);
 #endif
@@ -75,48 +73,13 @@ public:
         center_ = (pmp::vec3)bb.center();
         radius_ = 0.5f * bb.size();
 
-        gl_mesh_data_.vertex_array_.bind();
-
-        auto V = vertex_property(mesh_, mesh_.vertex_property<pmp::Point>("v:point"));
-        gl_mesh_data_.position_.update_float_buffer(V.transpose());
-
-        pmp::SurfaceNormals::compute_vertex_normals(mesh_);
-        auto VN = vertex_property(mesh_, mesh_.vertex_property<pmp::Normal>("v:normal"));
-        if (update_normal)
-            gl_mesh_data_.vnormal_.update_float_buffer(VN.transpose());
-
-        Eigen::Matrix<std::uint32_t, 3, -1> F(3, mesh_.n_faces());
-        for (auto f : mesh_.faces())
-        {
-            int f_i = 0;
-            for (auto v : mesh_.vertices(f))
-                F(f_i++, f.idx()) = v.idx();
-        }
-        gl_mesh_data_.triangles_.update_element_buffer(F);
-
-        Eigen::Matrix<std::uint32_t, 2, -1> E(2, mesh_.n_edges());
-        for (auto e : mesh_.edges())
-        {
-            E(0, e.idx()) = mesh_.from_vertex(mesh_.halfedge(e, 0)).idx();
-            E(1, e.idx()) = mesh_.from_vertex(mesh_.halfedge(e, 1)).idx();
-        }
-        gl_mesh_data_.edges_.update_element_buffer(E);
-
-        gl_mesh_data_.vertex_array_.unbind();
-        glCheckError();
+        mesh_.update_opengl_buffers();
     }
 
     //! draw the scene in different draw modes
     virtual void draw(const std::string& draw_mode) override
     {
-        phong_material_.use(projection_matrix_, modelview_matrix_);
-        gl_mesh_data_.gl_draw(GLMeshVertexArray::draw_triangles, gl_mesh_data_.triangles_);
-        phong_material_.shader_.set_uniform("front_color", pmp::vec3(0.1, 0.1, 0.1));
-        phong_material_.shader_.set_uniform("back_color", pmp::vec3(0.1, 0.1, 0.1));
-        phong_material_.shader_.set_uniform("use_lighting", false);
-        gl_mesh_data_.gl_draw(GLMeshVertexArray::draw_lines, gl_mesh_data_.edges_);
-
-        phong_material_.disable();
+        mesh_.draw(projection_matrix_, modelview_matrix_, draw_mode);
     }
 
     //! handle ImGUI interface
@@ -133,11 +96,9 @@ public:
     void do_processing() override;
 
 private:
-    pmp::SurfaceMesh mesh_;
+    pmp::SurfaceMeshGL mesh_;
     std::string filename_; //!< the current file
 
-    PhongMaterial phong_material_;
-    GLMeshPrimitive gl_mesh_data_;
     std::shared_ptr<MeshViewerData> data_;
 };
 
--- src/games102_hw6/main.cpp
+++ src/games102_hw6/main.cpp
@@ -23,61 +23,74 @@ struct MeshViewerData
     float lambda_{0.2};
 };
 
+static char* files[]{"Balls.obj", "Bunny_head.obj", "Cat_head.obj", "David328.obj", "Nefertiti_face.obj"};
+
 void MeshViewer::process_imgui()
 {
-    if (ImGui::Button("Update Normal"))
-        update_mesh(true);
-    ImGui::SameLine();
-    if (ImGui::Button("Reload Mesh"))
+    if (ImGui::CollapsingHeader("Minimal Area Surface"))
     {
-        data_ = nullptr;
-        load_mesh(filename_.data());
-        data_ = std::make_shared<MeshViewerData>(mesh_);
+#if __EMSCRIPTEN__
+        static int n = 4;
+        ImGui::Combo("Files", &n, files, 5);
+        ImGui::SameLine();
+#endif
+        if (ImGui::Button("Reload Mesh"))
+        {
+            data_ = nullptr;
+#if __EMSCRIPTEN__
+            load_mesh(files[n]);
+#else
+            load_mesh(filename_.data());
+#endif
+
+            data_ = std::make_shared<MeshViewerData>(mesh_);
+        }
+
+        ImGui::SliderFloat("lambda", &data_->lambda_, 0.01, 0.8);
+
+        ImGui::SliderInt("Implicit iters", &data_->implicit_iterations_, 1, 10);
+        if (ImGui::Button("Implicit Minimal Surf"))
+            data_->implicit_running_ = !data_->implicit_running_;
+
+        ImGui::SliderInt("Explicit iters", &data_->explicit_iterations_, 1, 200);
+        if (ImGui::Button("Explicit Minimal Surf"))
+            data_->explicit_running_ = !data_->explicit_running_;
+        ImGui::SameLine();
+        ImGui::Checkbox("Explicit Minimal Boundary", &data_->boundary_smoothing_);
     }
-
-    ImGui::SliderFloat("lambda", &data_->lambda_, 0.01, 0.8);
-
-    ImGui::SliderInt("Implicit iters", &data_->implicit_iterations_, 1, 10);
-    if (ImGui::Button("Implicit Minimal Surf"))
-        data_->implicit_running_ = !data_->implicit_running_;
-
-    ImGui::SliderInt("Explicit iters", &data_->explicit_iterations_, 1, 200);
-    if (ImGui::Button("Explicit Minimal Surf"))
-        data_->explicit_running_ = !data_->explicit_running_;
-    ImGui::SameLine();
-    ImGui::Checkbox("Explicit Minimal Boundary", &data_->boundary_smoothing_);
-
 }
 
-void MeshViewer::do_processing() {
-    if(data_ == nullptr)
+void MeshViewer::do_processing()
+{
+    if (data_ == nullptr)
         data_ = std::make_shared<MeshViewerData>(mesh_);
 
-    if (data_->explicit_running_ && data_->explicit_iterations_) {
+    if (data_->explicit_running_ && data_->explicit_iterations_)
+    {
         if (data_->boundary_smoothing_)
-            data_->minimal_surf_.boundary_explicit_iterate(data_->lambda_*0.5);
+            data_->minimal_surf_.boundary_explicit_iterate(data_->lambda_ * 0.5);
         data_->minimal_surf_.explicit_iterate(data_->lambda_);
-        data_->explicit_iterations_ --;
+        data_->explicit_iterations_--;
         update_mesh();
-    }else if(data_->explicit_running_)
+    }
+    else if (data_->explicit_running_)
     {
         data_->explicit_running_ = false;
         data_->explicit_iterations_ = 100;
     }
 
-
-    if (data_->implicit_running_ && data_->implicit_iterations_) {
+    if (data_->implicit_running_ && data_->implicit_iterations_)
+    {
         data_->minimal_surf_.implicit_iterate(data_->lambda_);
-        data_->implicit_iterations_ --;
+        data_->implicit_iterations_--;
         update_mesh();
-    }else if (data_->implicit_running_)
+    }
+    else if (data_->implicit_running_)
     {
         data_->implicit_running_ = false;
         data_->implicit_iterations_ = 5;
     }
 }
-
-
 
 } // namespace pupa_vis
 
@@ -85,6 +98,6 @@ int main(int argc, char* argv[])
 {
     pupa_vis::MeshViewer mesh_viewer("Games102-HW6", 800, 600);
 
-    mesh_viewer.load_mesh(argc == 2 ? argv[1] : "input.obj");
+    mesh_viewer.load_mesh(argc == 2 ? argv[1] : "Nefertiti_face.obj");
     mesh_viewer.run();
 }
